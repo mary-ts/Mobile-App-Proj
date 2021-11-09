c897bfb49b045e8b4298f6427c5284fc09f294d5
Mary Tease <58113541+mary-ts@users.noreply.github.com>
2021-11-09 17:41:00 +0000

    Update MainActivity
    its empty rn except for one button that does nothing-- will make it do something soon

--- MainActivity.java
+++ MainActivity.java
@@ -1,6 +1,5 @@
 package ie.ul.firebaseauthdemo;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -18,51 +17,8 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int RC_SIGN_IN = 123;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    public void OnClickSignIn (View view){
-        List<AuthUI.IdpConfig> providers = Arrays.asList(
-                new AuthUI.IdpConfig.EmailBuilder().build()
-        );
-
-        startActivityForResult(
-                AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(providers)
-                .build(), RC_SIGN_IN);
-    }
-
-
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == RC_SIGN_IN) {
-            IdpResponse response = IdpResponse.fromResultIntent(data);
-
-            if (resultCode == RESULT_OK) {
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                System.out.println("Sign in successful! \n" +
-                    "name = " + user.getDisplayName() + "\n" +
-                    "email = " + user.getEmail() + "\n" +
-                    "id = " + user.getUid()
-                        );
-            } else {
-                if (response == null) {
-                    System.out.println("Sign in cancelled");
-                    return;
-                }
-                if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
-                    System.out.println("No internet Connection");
-                    return;
-                }
-
-            }
-        }
-    }
-}
